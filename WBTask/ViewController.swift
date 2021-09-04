0415a69b025d3259a931d7eabf7818f3d1d006b0
Ugur Cetinkaya <ugur.cetinkaya@dpgmedia.nl>
2021-09-04 18:17:05 +0200

    Initial commit

---
+++ WBTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBTask
+//
+//  Created by Ugur on 02/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
