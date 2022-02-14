53eb5f65fad3950e57648d29805c4fd3dea80ab3
Nick Nguyen <50033125+nicknyn@users.noreply.github.com>
2022-02-14 16:00:40 -0500

    Initial Commit

---
+++ WBTakeHome/WBTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WBTakeHome
+//
+//  Created by Nick Nguyen on 2/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
